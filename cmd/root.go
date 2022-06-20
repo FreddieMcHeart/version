8cb4a1ddc11389f116ded43fad399b1e59755919
Nazarii <nazarii@starlink.ua>
2022-06-21 00:32:22 +0300

    Disabled completion command

--- cmd/root.go
+++ cmd/root.go
@@ -7,14 +7,15 @@ import (
 )
 
 var rootCmd = &cobra.Command{
-	Use:   "version",
-	Short: "Version is a very easy to use CLI tool generate versions",
-	Long:  `Version is a very easy to use CLI tool generate versions`,
+	Use: "version",
+	//Short: "Version is a very easy to use CLI tool generate versions",
+	Long: `Version is a very easy to use CLI tool generate versions`,
 }
 
 func Execute() {
+	rootCmd.CompletionOptions.DisableDefaultCmd = true
 	if err := rootCmd.Execute(); err != nil {
-		fmt.Fprintln(os.Stderr, err)
+		fmt.Println(err)
 		os.Exit(1)
 	}
 }
--- cmd/version.go
+++ cmd/version.go
@@ -13,7 +13,7 @@ func init() {
 var versionCmd = &cobra.Command{
 	Use:   "version",
 	Short: "Print the version number of Version",
-	Long:  `All software has versions. This is Version's`,
+	//Long:  `All software has versions. This is Version's`,
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Println("Version Generator v.alpha.0.0 -- HEAD")
 	},
